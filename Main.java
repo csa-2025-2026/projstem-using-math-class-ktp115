a2d77a48c0ab7230c4fdfa1d769d87d8fbe269e1
Katie <ktpham115@student.hbuhsd.edu>
2025-09-18 17:40:31 +0000

    1.11

--- Main.java
+++ Main.java
@@ -6,27 +6,53 @@ public class Main
     {
         // You can optionally use the Scanner for user input
         Scanner scan = new Scanner(System.in);
-        
-        int numOfNums;
-        System.out.println("How many random numbers do you want to see (must be at least 1)?");
-        numOfNums = scan.nextInt();
-        printRandom(numOfNums);
+        //1
+        int N;
+        System.out.println("Enter a positive integer: ");
+        N = scan.nextInt();
+        printRandom3(N);
+        //2
+        double x1;
+        double x2;
+        double y1;
+        double y2;
+
+        System.out.println("Enter the first x-coordinate: ");
+        x1 = scan.nextDouble();
+        System.out.println("Enter the second x-coordinate: ");
+        x2 = scan.nextDouble();
+        System.out.println("Enter the first y-coordinate: ");
+        y1 = scan.nextDouble();
+        System.out.println("Enter the second y-coordinate: ");
+        y2 = scan.nextDouble();
+
+        System.out.println("Slope: " + calcSlope(x1, x2, y1, y2););
+
     }
     
     public static void printRandom3(int num)
     {
-        // Do problem 1 here
+        //random whole num between a and b 
+        //(int)(Math.random()* (b-a+1)+a)
+        int lowerBound = 2;
+        int upperBound = num + 2;
+        System.out.println("Numbers: ");
+        System.out.println((int) (Math.random() * (upperBound - lowerBound + 1)+ lowerBound));
+        System.out.println((int) (Math.random() * (upperBound - lowerBound + 1)+ lowerBound));
+        System.out.println((int) (Math.random() * (upperBound - lowerBound + 1)+ lowerBound));
+
     }
     
     public static double calcSlope(double x1, double x2, double y1, double y2)
     {
-        // Do problem 2 here; replace code below
-        return 0.0;
+        double result = ((y2 - y1)/(x2 - x1));
+        return result;
     }
     
     public static int roundedDist(double a, double b)
     {
-        // Do problem 3 here; replace code below
-        return 0;
+        double dist = Math.abs(b - a);
+		int rounded = (int) (dist + 0.5);
+        return rounded;
     }
 }
